f3c6c68ce4cb087ca49b93417b0e7b112da2eac1
timlubanga <timlubanga@gmail.com>
2021-04-01 18:03:38 +0300

    configure postgres

--- ecommerceSite/settings.py
+++ ecommerceSite/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'ecommerceSite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'jumiaexpress',
+        'USER': 'timothy',
+        'PASSWORD': 'password123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
